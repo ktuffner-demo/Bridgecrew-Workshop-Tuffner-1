9efbef62a5d693ce43b0aecddba933f5a6a24d86
ktuffner-demo <98493117+ktuffner-demo@users.noreply.github.com>
2022-01-31 16:56:59 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,13 +1,17 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "tuffner-tf-test-bucket"
-  acl    = "private"
-}
-  versioning {
-    enabled = true
-  }
-}
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-03a0c45ebc70f98ea"
+  instance_type          = "t2.micro"
+  key_name               = "test1"
+  monitoring             = false
+  subnet_id              = "subnet-0a070c1dc78b28b3a"
+
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
