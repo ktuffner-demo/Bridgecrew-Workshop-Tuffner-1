9d3350cf402b48ae3b37e98678219b7e1fd1db85
ktuffner-demo <98493117+ktuffner-demo@users.noreply.github.com>
2022-01-31 16:46:02 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "b" {
   bucket = "tuffner-tf-test-bucket"
   acl    = "private"
+
   versioning {
     enabled = true
-  }
-}
+  
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
